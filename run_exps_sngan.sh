24eaf76ca302973e71ba2b9c4939065d51e574f3
limph0nimph <56155428+limph0nimph@users.noreply.github.com>
2021-12-07 17:47:43 +0300

    Update run_exps_sngan.sh

--- run_exps_sngan.sh
+++ run_exps_sngan.sh
@@ -3,3 +3,4 @@ python run_sngan.py  configs/exp_configs/inception_feature.yml  configs/gan_conf
 python run_sngan.py configs/exp_configs/inception_mean_discriminator.yml configs/gan_configs/sngan.yml
 python run_sngan.py configs/exp_configs/dumb_feature.yml configs/gan_configs/sngan.yml
 python run_sngan.py configs/exp_configs/discriminator_feature.yml  configs/gan_configs/sngan.yml
+python run_sngan.py configs/exp_configs/inception_mean_feature.yml  configs/gan_configs/sngan.yml
